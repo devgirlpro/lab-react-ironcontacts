baee9a90e544e845d36a8448c8708a78287a0ade
devgirl1992 <azadeh.galidari.1992@gmail.com>
2022-07-12 18:18:40 +0200

    work on delete button

--- src/App.js
+++ src/App.js
@@ -2,54 +2,6 @@ import "./App.css";
 import { useState } from "react";
 import contactData from "./contacts.json";
 
-// const contactsData = [
-//   {
-//     name: "Idris Elba",
-//     pictureUrl:
-//       "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-//     popularity: 11.622713,
-//     id: "11731993-0604-4bee-80d5-67ad845d0a38",
-//     wonOscar: false,
-//     wonEmmy: false,
-//   },
-//   {
-//     name: "Johnny Depp",
-//     pictureUrl:
-//       "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
-//     popularity: 15.656534,
-//     id: "7dad00f7-3949-477d-a7e2-1467fd2cfc06",
-//     wonOscar: false,
-//     wonEmmy: false,
-//   },
-//   {
-//     name: "Monica Bellucci",
-//     pictureUrl:
-//       "https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg",
-//     popularity: 16.096436,
-//     id: "0ad5e441-3084-47a1-9e9b-b917539bba71",
-//     wonOscar: false,
-//     wonEmmy: false,
-//   },
-//   {
-//     name: "Gal Gadot",
-//     pictureUrl:
-//       "https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg",
-//     popularity: 10.049256,
-//     id: "b497e3c4-50bb-4ae2-912f-eb36802c5bc2",
-//     wonOscar: false,
-//     wonEmmy: false,
-//   },
-//   {
-//     name: "Ian McKellen",
-//     pictureUrl:
-//       "https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg",
-//     popularity: 10.070132,
-//     id: "0067ae32-97b6-4431-898e-eb1c10150abb",
-//     wonOscar: false,
-//     wonEmmy: false,
-//   },
-// ];
-
 function App() {
   const size = 45;
   // const contactList = contactData.slice(0, size);
@@ -75,6 +27,11 @@ function App() {
     setContacts([...sortedByName]);
   };
 
+  const deleteHandler = (id) => {
+    console.log(id);
+    //write the code here
+  };
+
   return (
     <>
       <div className="btn">
@@ -89,9 +46,10 @@ function App() {
             <tr>
               <th>picture</th>
               <th>name</th>
-              <th>popularity</th>
-              <th>Won an Oscar</th>
-              <th>Won an Emmy</th>
+              <th>_popularity_</th>
+              <th>-Won an Oscar-</th>
+              <th>_Won an Emmy_</th>
+              <th>DELETE</th>
             </tr>
           </thead>
 
@@ -109,6 +67,9 @@ function App() {
                 <td>{contact.popularity}</td>
                 <td>{contact.wonOscar ? "🏆" : ""}</td>
                 <td>{contact.wonEmmy ? "🏆" : ""}</td>
+                <button onClick={() => deleteHandler(contact.id)}>
+                  Delete
+                </button>
               </tr>
             );
           })}
